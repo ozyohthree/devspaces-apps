94764547ce1260374d02448d9b33f2f29dbab4ba
Anthony G. <ozyohthree@gmail.com>
2025-10-29 15:45:04 -0400

    Updates to gitops app

--- gitops-config/monitoring/add-labels.sh
+++ gitops-config/monitoring/add-labels.sh
@@ -4,10 +4,10 @@
 oc label namespace openshift-devspaces openshift.io/cluster-monitoring=true
 
 # 2 Add label for Dev Workspace Operator Dashboard configmap. Verify In the Administrator view -> Observe → Dashboards → Dev Workspace Operator
-oc label configmap grafana-dashboard-dwo console.openshift.io/dashboard=true -n openshift-config-managed
+# oc label configmap grafana-dashboard-dwo console.openshift.io/dashboard=true -n openshift-config-managed
 
 # 3 Add label to devspaces grafana dashboard configmap
-oc label configmap grafana-dashboard-devspaces-server console.openshift.io/dashboard=true -n openshift-config-managed
+# oc label configmap grafana-dashboard-devspaces-server console.openshift.io/dashboard=true -n openshift-config-managed
 
 
 
